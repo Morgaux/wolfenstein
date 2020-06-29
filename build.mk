9524b7d5c5ea5c7ca9a15a84c55e1641aa0f399b
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-06-29 12:22:20 +1200

    Added comment corrections and increased verbosity

--- build.mk
+++ build.mk
@@ -6,10 +6,26 @@
 # These targets allow for the creation or fetching of any library dependencies
 # given in the ${LIB} variable, as well as any custom defined headerfiles.
 
+# The ${DRUMMY_FISH_LIBS} libraries are used for the rendering backend and are
+# frequently updated. To keep the sources for these upto date, these are pulled
+# from their git repositories rather than being included as part of this repo.
+# To ensure that the latest version is used, the repo subdirs are removed in the
+# 'clean' target and if the subdir exists, it is entered and a git pull is run
+# instead of the clone.
 ${DRUMMY_FISH_LIBS}: depends_on_git
-	cd $@ && git pull || git clone https://gitlab.com/drummyfish/$@
+	@echo "${YELLOW}Fetching $@...${RESET}"
+	@if cd $@ ;                                          \
+	then                                                 \
+		git pull ;                                   \
+	else                                                 \
+		git clone https://gitlab.com/drummyfish/$@ ; \
+	fi
 
+# The config.h file is used to define user level configuration that isn't
+# available at run time. The default values are provided in config.def.h,
+# however, to avoid overwriting the defaults, an untracked copy is used.
 config.h: depends_on_sed
+	@echo "${YELLOW}Creating $@...${RESET} [modify as needed]"
 	${SUBSTITUTE} < config.def.h > $@
 
 # }}}
@@ -18,10 +34,21 @@ config.h: depends_on_sed
 # These targets define the compilation and interrelated dependencies of the
 # object files defined in ${OBJ} that are used to build the final executable.
 
-.c.o:
+# This rule defines the compilation of the individual C modules to object files,
+# these are later linked together to for the final ${BIN} target, or may be
+# combined individually with test object files to create unit tests. Note that
+# any .c file requires a matching .h header to create the given .o file.
+%.o: %.c %.h
+	@echo "${YELLOW}Building $@...${RESET}"
 	${CC} -c ${CFLAGS} $<
 
-${OBJ}: config.h config.mk ${LIB}
+
+# This rule defines the dependency of any given object file on the config.h file
+# and the ${LIB} dirs, this trigger the creation of the config.h file if it has
+# been cleaned and triggers an error if a library directory is missing. This is
+# preferable to a linker failure as it is more descriptive of any OS or
+# architecture differences in library management.
+${OBJ}: config.h ${LIB}
 
 # }}}
 
@@ -29,7 +56,11 @@ ${OBJ}: config.h config.mk ${LIB}
 # These targets define the related ${OBJ} targets needed to build ${BIN} and
 # provides the implementation to do so.
 
+# This is the build rule for ${BIN} which creates a binary executable for
+# installation or testing. All ${OBJ} files are compiled separately in the
+# OBJECT COMPILATION TARGETS listed above.
 ${BIN}: ${OBJ}
+	@echo "${YELLOW}Linking $@...${RESET}"
 	${CC} -o $@ $^ ${LDFLAGS}
 
 # }}}
@@ -38,7 +69,10 @@ ${BIN}: ${OBJ}
 # These targets define the creation of the related ${MAN} page of the ${BIN}
 # target.
 
+# This target generates a troff manpage for installation of a manpage for
+# ${BIN}. This follows the standard Unix documentation conventions.
 ${MAN}:
+	@echo "${YELLOW}Generating ${MAN} manpage...${RESET}"
 	@{                                                                     \
 		echo "${MAN_COMMENT} Manpage for ${BIN}"                     ; \
 		echo "${MAN_COMMENT} Contact ${CONTACT}"                       \
@@ -65,4 +99,6 @@ ${MAN}:
 	} | ${CREATE} $@
 
 # }}}
+
+.PHONY: ${LIBS}
 
--- dependencies.mk
+++ dependencies.mk
@@ -2,21 +2,31 @@
 # Programmatically check for programs the build depends on
 #
 
+# This variable defines any external programs that should be checked for, to add
+# this as a check, add 'depends_on_FOO' as a dependency for the target in
+# question, where FOO is defined in ${DEPENDENCIES}. This will trigger the check
+# target for that tool which will provide a clear error message and halt the
+# build progress.
 DEPENDENCIES := git sed tar gzip
-_DEP_CHECKS_ := ${DEPENDENCIES:%=depends_on_%}
 
-# do nothing if invoked
+# This NOOP target prevents any of the ${DEPENDENCIES} being invoked as
+# dependency by the checking target, as it uses '%' stem syntax to derive 'FOO'
+# from 'depends_on_FOO'.
 ${DEPENDENCIES}: ;
 
-${_DEP_CHECKS_}: depends_on_% : %
+# This target is run for every call of 'depends_on_FOO' for 'FOO's in
+# ${DEPENDENCIES}, passing 'FOO' as $<. This allow a shell to check for 'FOO' in
+# the current system $PATH. If missing or not found to be executable, a message
+# is shown.
+${DEPENDENCIES:%=depends_on_%}: depends_on_% : %
 	@if [ ! -x "$$(command -v $<)" ] ;          \
-	then                                         \
-		echo "${RED}ERROR:${RESET}"          \
-		     "${BOLD}${BIN}${RESET}"         \
-		     "depends on"                    \
+	then                                        \
+		echo "${RED}ERROR:${RESET}"         \
+		     "${BOLD}${BIN}${RESET}"        \
+		     "depends on"                   \
 		     "${YELLOW}${BOLD}$<${RESET}" ; \
-		exit 1 ;                             \
+		exit 1 ;                            \
 	fi
 
-.PHONY: ${DEPENDENCIES} ${_DEP_CHECKS_}
+.PHONY: ${DEPENDENCIES} ${DEPENDENCIES:%=depends_on_%}
 
