34ed85815e61b14fe2f4e47c7f873f7d6b577ac0
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-07-01 17:02:52 +1200

    Begin simple dummy implemementation of wolfenstein3D.c

--- wolfenstein3D.c
+++ wolfenstein3D.c
@@ -3,9 +3,22 @@
  * TODO: implement everything
  */
 
+/**
+ * Include config.h, this should be done first, before any other includes or
+ * definitions. This allows for the user configurable settings to be accessible
+ * to the local code and headers.
+ */
+#include "config.h"
+
+/**
+ * Include any external libraries and system headers here, in order.
+ */
 #include <stdio.h>
 
-#include "config.h"
+/**
+ * Include definitions and prototypes for main wolfenstein3D.c source.
+ */
+#include "wolfenstein3D.h"
 
 int main(int argc, char ** argv) {
 	printf("VERSION=%s\n", VERSION);
--- wolfenstein3D.h
+++ wolfenstein3D.h
@@ -1,3 +1,34 @@
 /**
  * Header file for wolfenstein3D.c
  */
+
+/* RAYCASTLIB DEFINITIONS {{{ */
+
+/**
+ * Tell raycastlib the name of the function with which we write pixels to the
+ * screen.
+ */
+#define RCL_PIXEL_FUNCTION pixelFunc
+
+/**
+ * Turn off what we don't need, to improve performance.
+ */
+#define RCL_COMPUTE_FLOOR_DEPTH   0
+#define RCL_COMPUTE_CEILING_DEPTH 0
+
+/**
+ * Define screen dimensions.
+ */
+#define SCREEN_W 80
+#define SCREEN_H 40
+#define PIXELS_TOTAL ((SCREEN_W + 1) * SCREEN_H + 1)
+
+/* }}} */
+
+/**
+ * Define screen size accounting for terminating NULL and \n characters.
+ */
+char screen[(SCREEN_W + 1) * SCREEN_H + 1];
+
+int main(int argc, char ** argv);
+
