4a917025e91c05cc77038ebfdad221c2cb13603a
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-07-10 00:13:46 +0000

    Nuke the config file for now, wait until we have something to configure

--- config.def.h
+++ config.def.h
@@ -7,22 +7,5 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* DEFINE CONFIGURATION SYMBOLS {{{ */
-
-/**
- * Define screen dimensions.
- */
-#define SCREEN_W 80
-#define SCREEN_H 40
-#define PIXELS_TOTAL ((SCREEN_W + 1) * SCREEN_H + 1)
-
-/**
- * Define map dimensions.
- */
-#define MAP_W 15
-#define MAP_H 20
-
-/* }}} */
-
 #endif
 
