f3f1d395c1acc5a97a74bf6181404804d1077b8f
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-07-20 13:37:56 +1200

    Added assertion macros to the testing makefile

--- tests.mk
+++ tests.mk
@@ -2,6 +2,29 @@
 # This file defines the main test cases and testing structures
 #
 
+# TEST MACROS {{{
+# This section contains macros for use with test cases, especially 'assertion'
+# statements that allow more programmatic and readable test cases.
+
+# These are simple wrappers that encapsulate the test(1) utility on UNIX boxes.
+ASSERT_TRUE  := echo "${YELLOW}[ASSERTION]${RESET}" ; test
+ASSERT_FALSE := echo "${YELLOW}[NEGATIVE ASSERTION]${RESET}" ; test !
+
+# These assertions are for testing and asserting the specific properties of
+# given files by local or absolute file name.
+ASSERT_FILE_EXISTS      := echo "${YELLOW}[ASSERT EXISTS]${RESET}"     ; test -f
+ASSERT_DIRECTORY_EXISTS := echo "${YELLOW}[ASSERT DIR EXISTS]${RESET}" ; test -d
+ASSERT_FILE_EXECUTABLE  := echo "${YELLOW}[ASSERT EXECUTABLE]${RESET}" ; test -x
+ASSERT_FILE_READABLE    := echo "${YELLOW}[ASSERT READABLE]${RESET}"   ; test -r
+ASSERT_FILE_NOT_EMPTY   := echo "${YELLOW}[ASSERT NOT EMPTY]${RESET}"  ; test -s
+
+# These are the assertion macros for testing properties of shell strings, these
+# may be expanded make(1) variables or shell expression results.
+ASSERT_STRING_EMPTY     := echo "${YELLOW}[ASSERT EMPTY]${RESET}"     ; test -z
+ASSERT_STRING_NOT_EMPTY := echo "${YELLOW}[ASSERT NOT EMPTY]${RESET}" ; test -n
+
+# }}}
+
 # TEST CONFIG {{{
 
 # This variable defines all test case names, for each test case a matching
@@ -135,18 +158,22 @@ ALL_TEST_TARGETS := ${TEST_ACTIONS}                                            \
 run_test_build:
 	mkdir -p tmp
 	make DESTDIR=tmp clean config all install uninstall dist
-	test -f tmp${BIN_DIR}/${BIN}
+	${ASSERT_DIRECTORY_EXISTS} tmp${BIN_DIR}/
+	${ASSERT_DIRECTORY_EXISTS} tmp${MAN_DIR}/
+	${ASSERT_FILE_EXECUTABLE}  tmp${BIN_DIR}/${WOLF_3D}
+	${ASSERT_FILE_EXISTS}      tmp${MAN_DIR}/${WOLF_3D:%=%.1}
+	${ASSERT_FILE_READABLE}    tmp${MAN_DIR}/${WOLF_3D:%=%.1}
+	${ASSERT_FILE_NOT_EMPTY}   tmp${MAN_DIR}/${WOLF_3D:%=%.1}
 	${CLEAN}
 	${DELETE} tmp
-	test ! -d tmp
+	${ASSERT_FALSE} -d tmp
 
 ${DRUMMY_FISH_LIBS:%=run_test_%}: run_test_% : %
-	test -d $<
-	test -d $</programs
-	test -f $</programs/make.sh
-	test -f $</programs/helloWorld.c
+	${ASSERT_DIRECTORY_EXISTS} $<
+	${ASSERT_DIRECTORY_EXISTS} $</programs
+	${ASSERT_FILE_EXISTS} $</programs/helloWorld.c
 	cd $</programs && ${CC} ${CFLAGS} -o helloWorld helloWorld.c
-	test -x $</programs/helloWorld
+	${ASSERT_FILE_EXECUTABLE} $</programs/helloWorld
 	./$</programs/helloWorld
 
 ${MODULES:%=run_test_%}:
